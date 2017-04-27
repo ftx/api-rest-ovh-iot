c4e5ff78fdf95ecb7dbb08dc3415250dd89b6be7
Florian Mauduit <flotix@linux.com>
2017-04-27 16:35:23 +0200

    Add ovh iot

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var port = process.env.PORT || 96;
 
+var TOKEN_ID = process.env.TOKEN_ID;
+var TOKEN_KEY = process.env.TOKEN_KEY;
 
-// routes will go here
+// routes
+app.post('/', function(req, res) {
+	res.write('REST API NodeJS IOT OVH (sigfox version) - flotix@linux.com');
+});
+
+
+
 app.post('/send', function(req, res) {
     var temp = req.body.slot_temp;
     var hum = req.body.slot_hum;
@@ -14,6 +22,61 @@ app.post('/send', function(req, res) {
 
     res.send(signal + ' ' + temp + ' ' + hum);
     console.log(signal + ' ' + temp + ' ' + hum);
+
+
+//JSON
+var data = [{
+			metric:"temp",
+			value:temp,
+			tags:{
+				source:"sigfox",
+				region:"Le Bouscat"
+			}
+},
+{
+                        metric:"hum",
+                        value:hum,
+                        tags:{
+                                source:"sigfox",
+                                region:"Le Bouscat"
+                        }
+},
+{
+                        metric:"signal",
+                        value:signal,
+                        tags:{
+                                source:"sigfox",
+                                region:"Le Bouscat"
+                        }
+}
+];
+
+
+// Send data
+request({
+	uri: "https://opentsdb-gra1.tsaas.ovh.com/api/put",
+	auth: {
+		user: TOKEN_ID,
+		pass: TOKEN_KEY,
+		sendImmediately: true
+	},
+	method: "POST",
+	json: data
+}, function (error, response, body) {
+	if (error) {
+		console.log(error);
+	} else if (response.statusCode >= 400) {
+		console.log(body);
+		console.log(response.statusMessage);
+	} else {
+		console.log("...Success! Send to OVH IOT...");
+	}
+});
+
+
+
+
+
 });
 
 
